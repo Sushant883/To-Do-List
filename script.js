28e7640c25280518ac55b7df692f784fc10c34bd
Sushant883 <panditsushant11@gmail.com>
2024-11-02 20:43:04 +0530

    write the js code for inputbox value

---
+++ script.js
@@ -0,0 +1,20 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+
+//in this function we are adding the task to the list.
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You must write something!");
+    }
+    else {
+        let li = document.createElement("li");// create li tag
+        li.innerHTML = inputBox.value;// compare the value of li(tag) and inputBox.value
+        listContainer.appendChild(li);// display the li(tag)value in the output.
+
+        let span = document.createElement("Span");// create span tag for (X) sign.
+        span.innerHTML = "\u00d7"// create the X sign
+        li.appendChild(span);// display the X sign
+    }
+    inputBox.value = "";
+}
