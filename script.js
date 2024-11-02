7bd52aa7f6d7fbbedff9ec52f5449818e7c297b6
Sushant883 <panditsushant11@gmail.com>
2024-11-02 20:55:59 +0530

    Completer the code of js

--- script.js
+++ script.js
@@ -23,12 +23,18 @@ function addTask() {
 listContainer.addEventListener("click", function(e) {
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
+        saveData()
     }
     else if (e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
+        saveData()
     }
 }, false);
 
 function saveData(){
     localStorage.setItem("data", listContainer.innerHTML);// save the data when brower is refresh
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
 }
